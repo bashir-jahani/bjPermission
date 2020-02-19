6e55d433dbd126794f150a02e9432499da9141e5
bashir <bashir.jahani@live.com>
2020-02-19 19:38:34 +0330

    match with github

---
+++ src/main/java/bj/modules/bj_permission.java
@@ -0,0 +1,154 @@
+package bj.modules;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.StringRes;
+
+import bj.modules.bj_messageBox_objcets.messageBox;
+import bj.modules.bj_permission_objects.permissionCreateDialog;
+import bj.modules.bj_permisson.R;
+
+import static androidx.core.app.ActivityCompat.requestPermissions;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
+import static bj.modules.bj_messageBox.BJMessagesButtonKind.*;
+import static bj.modules.bj_messageBox.MessageBox;
+
+public class bj_permission {
+	public interface GetPermissionListener{
+		void onPermissionProcesComplated(String PermissionNeeded, Boolean HavePermission);
+	}
+	public static boolean HavePermission(Context context, String PermissionNeeded){
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+				&& PermissionNeeded != null) {
+			if (  checkSelfPermission(context, PermissionNeeded) == PackageManager.PERMISSION_GRANTED) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return true;
+		}
+	}
+
+	public static boolean CheckPermision(Context context, String PermissionNeeded,String   PermissionNeedDescription,GetPermissionListener getPermissionListener){
+		return permissionCreateDialog.openMe(context,PermissionNeeded,PermissionNeedDescription,getPermissionListener);
+	}
+	public static boolean CheckPermision(Context context, String PermissionNeeded, @StringRes int PermissionNeedDescriptionResourseID, GetPermissionListener getPermissionListener){
+		return permissionCreateDialog.openMe(context,PermissionNeeded,context.getResources().getString(PermissionNeedDescriptionResourseID),getPermissionListener);
+	}
+
+	public static void GetPermissions(Context context, final Activity activety, final String PermissionNeeded, @StringRes int PermissionNeedDescriptionResourseID, final Boolean WaitUntil, GetPermissionListener getPermissionListener)
+	{
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+				&& PermissionNeeded != null) {
+			//Toast.makeText(G.MyContext, (ActivityCompat.checkSelfPermission(G.MyContext, PermissionNeeded) != PackageManager.PERMISSION_GRANTED)+"\n"+ActivityCompat.checkSelfPermission(G.MyContext, PermissionNeeded) + "==" + PackageManager.PERMISSION_GRANTED, Toast.LENGTH_SHORT).show();
+
+			if (checkSelfPermission(context, PermissionNeeded) != PackageManager.PERMISSION_GRANTED) {
+
+				if (WaitUntil) {
+					Log.e("GGN","Cant wait for permission");
+					try{
+
+						MessageBox(R.string.title_Permission,PermissionNeedDescriptionResourseID,Yes_No,context,new messageBox.OnDialogResultListener(){
+							@Override
+							public boolean OnResult(Boolean dialogResult) {
+								if (dialogResult) {
+									try{
+										requestPermissions(activety, new String[]{PermissionNeeded}, 1);
+									}catch (Exception e1){
+										Log.e("GGN","Permision Error 11:"+ e1.getMessage());
+									}
+									return true;
+								}else {
+									return false;
+								}
+
+							}
+						});
+					}catch (Exception e){
+						Log.e("GGN","Permision Error 1:"+ e.getMessage());
+					}
+
+				}else {
+					try{
+						MessageBox(R.string.title_Permission,PermissionNeedDescriptionResourseID, Yes_No,context,new  messageBox.OnDialogResultListener(){
+							@Override
+							public boolean OnResult(Boolean dialogResult) {
+								if (dialogResult) {
+									try{
+										requestPermissions(activety, new String[]{PermissionNeeded}, 1);
+									}catch (Exception e1){
+										Log.e("GGN","Permision Error 22:"+ e1.getMessage());
+									}
+
+									return true;
+								}else {
+									return false;
+								}
+
+							}
+						});
+					}catch (Exception e){
+						Log.e("GGN","Permision Error 2:"+ e.getMessage());
+					}
+				}
+
+
+			}
+		}
+	}
+	public static void GetPermissions(final Context context, final Activity activety, final String PermissionNeeded, String PermissionNeedDescription, final Boolean WaitUntil)
+	{
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+				&& PermissionNeeded != null) {
+			if (checkSelfPermission(context, PermissionNeeded) != PackageManager.PERMISSION_GRANTED) {
+				if (WaitUntil) {
+					Log.e("GGN","Cant wait for permission");
+					try{
+						MessageBox(context.getResources().getString(R.string.title_Permission),PermissionNeedDescription, Yes_No,context,new messageBox.OnDialogResultListener(){
+							@Override
+							public boolean OnResult(Boolean dialogResult) {
+								if (dialogResult) {
+									requestPermissions(activety, new String[]{PermissionNeeded}, 1);
+									return true;
+								}else {
+									return false;
+								}
+
+							}
+						});
+					}catch (Exception e){
+						Log.e("GetPermissions 1","GetPermissions Error: "+e.getMessage());
+					}
+
+				}else {
+					try{
+						MessageBox(context.getResources().getString(R.string.title_Permission),PermissionNeedDescription, Yes_No,context,new messageBox.OnDialogResultListener(){
+							@Override
+							public boolean OnResult(Boolean dialogResult) {
+
+								if (dialogResult) {
+									requestPermissions( activety, new String[]{PermissionNeeded}, 1);
+
+									return true;
+								}else {
+									return false;
+								}
+
+							}
+						});
+					}catch (Exception e){
+						Log.e("GetPermissions 11","GetPermissions Error: "+e.getMessage());
+					}
+				}
+
+
+			}
+		}
+	}
+}
---
+++ src/main/java/bj/modules/bj_permission_objects/permissionCreateDialog.java
@@ -0,0 +1,106 @@
+package bj.modules.bj_permission_objects;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+
+
+import java.util.Random;
+
+import bj.modules.bj_permission.*;
+import bj.modules.bj_permisson.R;
+
+import static bj.modules.bj_permission.*;
+
+public class permissionCreateDialog extends AppCompatActivity {
+	TextView TXVPermission,TXVPermissionDSC;
+	Button BTNYes,BTNNo;
+	private static String permissionNeeded,  permissionNeedDescription;
+	private static Context context;
+	private TextView TXVTitle;
+	public static GetPermissionListener _getPermissionListener;
+	public static String TitleSet;
+	private static int MY_PERMISSIONS_REQUEST_READ_CONTACTS;
+
+	public static boolean openMe(Context context, String PermissionNeeded, String PermissionNeedDescription, GetPermissionListener getPermissionListener){
+		TitleSet=context.getResources().getString(R.string.title_Permission);
+
+			if (!HavePermission(context,PermissionNeeded)) {
+				permissionNeeded = PermissionNeeded;
+				permissionNeedDescription = PermissionNeedDescription;
+				permissionCreateDialog.context = context;
+				_getPermissionListener=getPermissionListener;
+				Random r = new Random();
+				MY_PERMISSIONS_REQUEST_READ_CONTACTS= r.nextInt(100) + 10;
+
+				Intent intent = new Intent(context, permissionCreateDialog.class);
+				context.startActivity(intent);
+
+				return false;
+			}else {
+				if (getPermissionListener!=null){
+					getPermissionListener.onPermissionProcesComplated(PermissionNeeded,true);
+				}
+				return true;
+			}
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_permission_create_dialog);
+
+		TXVPermission=findViewById(R.id.APCDTXVPermission);
+		TXVPermissionDSC=findViewById(R.id.APCDTXVPermission);
+		TXVTitle=findViewById(R.id.APCDTXVTitle);
+		BTNNo=findViewById(R.id.APCDBTNNo);
+		BTNYes=findViewById(R.id.APCDBTNYes);
+
+		TXVPermissionDSC.setText(permissionNeedDescription);
+		TXVPermission.setText(permissionNeeded);
+		TXVTitle.setText(TitleSet);
+		BTNNo.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				finish();
+				Log.i("*************************","_getPermissionListener is nul: "+(_getPermissionListener==null));
+				if (_getPermissionListener!=null){
+					_getPermissionListener.onPermissionProcesComplated(permissionNeeded, HavePermission(context,permissionNeeded));
+				}
+			}
+		});
+		BTNYes.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				ActivityCompat.requestPermissions(permissionCreateDialog.this,
+						new String[]{permissionNeeded},
+						MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+			}
+		});
+
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		if (requestCode==MY_PERMISSIONS_REQUEST_READ_CONTACTS){
+			finish();
+			if (_getPermissionListener != null) {
+				_getPermissionListener.onPermissionProcesComplated(permissionNeeded, HavePermission(context, permissionNeeded));
+			}
+
+		}
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+	}
+}
+
